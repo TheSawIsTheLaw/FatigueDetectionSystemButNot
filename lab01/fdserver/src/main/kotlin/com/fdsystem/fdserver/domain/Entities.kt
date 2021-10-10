4e06e7ca61401cabe3db678da89134a2b27bad45
TheSawIsTheLaw <dimakrok@bk.ru>
2021-10-10 13:34:20 +0300

    ADD: init values

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/domain/Entities.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/domain/Entities.kt
@@ -1,23 +1,23 @@
 package com.fdsystem.fdserver.domain
 
-data class ArterialPressure(val topPressure: Int = 0, val botPressure: Int = 0)
+data class ArterialPressure(val topPressure: Int = -1, val botPressure: Int = -1)
 
-data class Pulse(val pulse: Int)
+data class Pulse(val pulse: Int = -1)
 
-data class Breathe(val rate: Float, val type: String)
+data class Breathe(val rate: Float = -1.0f, val type: String = "")
 
-data class Cortisol(val cortLevel: Float)
+data class Cortisol(val cortLevel: Float = -1.0f)
 
-data class MouseClicks(val clicksRate: Float)
+data class MouseClicks(val clicksRate: Float = -1.0f)
 
-data class MouseSpeed(val speed: Float)
+data class MouseSpeed(val speed: Float = -1.0f)
 
-data class DHEA(val level: Float)
+data class DHEA(val level: Float = -1.0f)
 
-data class PrintSpeed(val speed: Float)
+data class PrintSpeed(val speed: Float = -1.0f)
 
-data class BlinkRate(val rate: Float)
+data class BlinkRate(val rate: Float = -1.0f)
 
-data class FacialExpression(val expression: String)
+data class FacialExpression(val expression: String = "")
 
-data class SpeechRate(val rate: Float)
+data class SpeechRate(val rate: Float = -1.0f)
