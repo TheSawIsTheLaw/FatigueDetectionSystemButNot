26fcb5d9337b57742abd4e605abd1929d6fcf664
TheSawIsTheLaw <dimakrok@bk.ru>
2021-10-10 18:58:06 +0300

    ADD: new interface for repository. Idk, looks like shit, you know

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/data/CharRepositoryImpl.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/data/CharRepositoryImpl.kt
@@ -2,8 +2,12 @@ package com.fdsystem.fdserver.data
 
 import com.fdsystem.fdserver.domain.CharRepositoryInterface
 import com.fdsystem.fdserver.domain.Characteristics
+import com.influxdb.client.domain.HealthCheck
 import com.influxdb.client.kotlin.InfluxDBClientKotlin
 import com.influxdb.client.kotlin.InfluxDBClientKotlinFactory
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.runBlocking
+import java.time.Instant
 
 class InfluxConnection(connectionString_: String, token_: String, org_: String)
 {
@@ -35,12 +39,40 @@ class CharRepositoryImpl(connectionString: String, token: String, org: String): 
 {
     private val connection = InfluxConnection(connectionString, token, org)
 
-    override fun get(subjectName: String, timeRange: Pair<Int, Int>): List<Characteristics>
+    override fun get(subjectName: String, timeRange: Pair<Int, Int>): List<Triple<String, String, Instant>>
+    {
+        if (connection.getConnectionToDB().health().status == HealthCheck.StatusEnum.FAIL)
+            return listOf()
+
+        val outList: MutableList<Triple<String, String, Instant>> = mutableListOf()
+        val client = connection.getConnectionToDB()
+
+        val rng =
+            if (timeRange.second == 0) "start: ${timeRange.first}" else "start: ${timeRange.first}, stop: ${timeRange.second}}"
+        val query = ("from(bucket: \"$subjectName\")\n" +
+                "|> range($rng)")
+        val result = client.getQueryKotlinApi().query(query)
+
+        val tempList =
+        runBlocking {
+            for (i in result)
+            {
+                val curVal = i.values
+                outList.add(Triple(curVal["_measurement"].toString(), curVal["_value"].toString(), curVal["_time"] as Instant))
+            }
+        }
+        connection.closeConnection()
+
+        return outList.toList()
+    }
+
+    override fun get(subjectName: String, charName: String,
+                     timeRange: Pair<Int, Int>): List<Triple<String, String, Instant>>
     {
         TODO("Not yet implemented")
     }
 
-    override fun add(subjectName: String, chars: List<Characteristics>)
+    override fun add(subjectName: String, chars: List<Triple<String, String, Instant>>)
     {
         TODO("Not yet implemented")
     }
--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/domain/CharRepositoryInterface.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/domain/CharRepositoryInterface.kt
@@ -1,8 +1,11 @@
 package com.fdsystem.fdserver.domain
 
+import java.time.Instant
+
 
 interface CharRepositoryInterface
 {
-    fun get(subjectName: String, timeRange: Pair<Int, Int>): List<Characteristics>
-    fun add(subjectName: String, chars: List<Characteristics>)
+    fun get(subjectName: String, timeRange: Pair<Int, Int>): List<Triple<String, String, Instant>>
+    fun get(subjectName: String, charName: String, timeRange: Pair<Int, Int>): List<Triple<String, String, Instant>>
+    fun add(subjectName: String, chars: List<Triple<String, String, Instant>>)
 }
