d948001f604c7bc7d1c12ebf53eef874c8816e88
TheSawIsTheLaw <dimakrok@bk.ru>
2021-10-10 19:32:43 +0300

    ADD: method for write and separated connection for write

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/data/CharRepositoryImpl.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/data/CharRepositoryImpl.kt
@@ -1,11 +1,10 @@
 package com.fdsystem.fdserver.data
 
 import com.fdsystem.fdserver.domain.CharRepositoryInterface
-import com.fdsystem.fdserver.domain.Characteristics
 import com.influxdb.client.domain.HealthCheck
+import com.influxdb.client.domain.WritePrecision
 import com.influxdb.client.kotlin.InfluxDBClientKotlin
 import com.influxdb.client.kotlin.InfluxDBClientKotlinFactory
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 import java.time.Instant
 
@@ -15,6 +14,7 @@ class InfluxConnection(connectionString_: String, token_: String, org_: String)
     private val token = token_
     private val org = org_
     private var connection: InfluxDBClientKotlin
+    private lateinit var writeApiConnection: InfluxDBClientKotlin
 
     init
     {
@@ -24,14 +24,32 @@ class InfluxConnection(connectionString_: String, token_: String, org_: String)
 
     fun getConnectionToDB(): InfluxDBClientKotlin
     {
-        connection = InfluxDBClientKotlinFactory
-            .create(connectionString, token.toCharArray(), org)
+        if (connection.health().status == HealthCheck.StatusEnum.FAIL)
+        {
+            connection = InfluxDBClientKotlinFactory
+                .create(connectionString, token.toCharArray(), org)
+        }
         return connection
+    }
+
+    fun getConnectionWrite(bucketName: String): InfluxDBClientKotlin
+    {
+        if (writeApiConnection.health().status == HealthCheck.StatusEnum.FAIL)
+        {
+            writeApiConnection = InfluxDBClientKotlinFactory
+                .create(connectionString, token.toCharArray(), org, bucketName)
+        }
+        return writeApiConnection
     }
 
     fun closeConnection()
     {
         connection.close()
+    }
+
+    fun closeWriteConnection()
+    {
+        writeApiConnection.close()
     }
 }
 
@@ -39,7 +57,8 @@ class CharRepositoryImpl(connectionString: String, token: String, org: String) :
 {
     private val connection = InfluxConnection(connectionString, token, org)
 
-    override fun get(subjectName: String, timeRange: Pair<Int, Int>, charName: String): List<Triple<String, String, Instant>>
+    override fun get(subjectName: String, timeRange: Pair<Int, Int>,
+                     charName: String): List<Triple<String, String, Instant>>
     {
         if (connection.getConnectionToDB().health().status == HealthCheck.StatusEnum.FAIL)
             return listOf()
@@ -49,7 +68,7 @@ class CharRepositoryImpl(connectionString: String, token: String, org: String) :
 
         val rng =
             if (timeRange.second == 0) "start: ${timeRange.first}" else "start: ${timeRange.first}, stop: ${timeRange.second}}"
-        var query: String  = "from(bucket: \"$subjectName\")\n" +
+        var query: String = "from(bucket: \"$subjectName\")\n" +
                 "|> range($rng)"
         if (charName.isNotBlank())
         {
@@ -70,8 +89,17 @@ class CharRepositoryImpl(connectionString: String, token: String, org: String) :
         return outList.toList()
     }
 
-    override fun add(subjectName: String, chars: List<Triple<String, String, Instant>>)
+    override fun add(subjectName: String, chars: List<Pair<String, String>>)
     {
-        TODO("Not yet implemented")
+        val client = connection.getConnectionWrite(subjectName)
+        val writeApi = client.getWriteKotlinApi()
+
+        runBlocking {
+            for (i in chars)
+            {
+                writeApi.writeRecord("${i.first}=${i.second}", WritePrecision.S)
+            }
+        }
+        connection.closeWriteConnection()
     }
 }
