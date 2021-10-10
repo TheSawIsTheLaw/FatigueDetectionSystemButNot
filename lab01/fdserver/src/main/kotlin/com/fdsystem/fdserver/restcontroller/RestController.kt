4ddf68b99d982aee56f0ef023c5a90367d72c240
TheSawIsTheLaw <dimakrok@bk.ru>
2021-10-10 15:08:51 +0300

    ADD: basics of api

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/restcontroller/RestController.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/restcontroller/RestController.kt
@@ -1,14 +1,11 @@
 package com.fdsystem.fdserver.restcontroller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.data.annotation.Id
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 
 @Table("MESSAGES")
 data class Message(@Id val id: String?, val text: String)
@@ -21,22 +18,64 @@ interface MessageRepository : CrudRepository<Message, String>
 }
 
 @Service
-class MessageService(val db: MessageRepository) {
+class MessageService(val db: MessageRepository)
+{
 
     fun findMessages(): List<Message> = db.findMessages()
 
-    fun post(message: Message){
+    fun post(message: Message)
+    {
         db.save(message)
     }
 }
 
 @RestController
-class MessageResource(val service: MessageService) {
-    @GetMapping("/api")
+class MessageController(val service: MessageService)
+{
+    @GetMapping("/messages")
     fun index(): List<Message> = service.findMessages()
 
-    @PostMapping("/api")
-    fun post(@RequestBody message: Message) {
+    @PostMapping("/messages")
+    fun post(@RequestBody message: Message)
+    {
         service.post(message)
+    }
+}
+
+@RestController
+class UserController()
+{
+    @GetMapping("/user/login")
+    fun login()
+    {
+    }
+
+    @GetMapping("/user/logout")
+    fun logout()
+    {
+    }
+
+    @GetMapping("/user/data")
+    fun getData()
+    {
+    }
+
+    @PostMapping("/user/data")
+    fun addData()
+    {
+    }
+}
+
+@RestController
+class SubjectController()
+{
+    @PostMapping("/subject")
+    fun createNewBucket()
+    {
+    }
+
+    @DeleteMapping("/subject")
+    fun deleteBucket()
+    {
     }
 }
