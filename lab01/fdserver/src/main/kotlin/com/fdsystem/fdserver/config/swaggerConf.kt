6dade9defc3836ca7cf3276e0241d88018abcad4
trvehazzk3r <dimakrok@bk.ru>
2021-10-14 01:54:20 +0300

    ADD: redirection to DB and we say goodbye to swagger-ui

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/config/swaggerConf.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/config/swaggerConf.kt
@@ -2,6 +2,9 @@ package com.fdsystem.fdserver.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -11,21 +14,35 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+
 // http://localhost:8080/swagger-ui/
 
 @Configuration
 @EnableSwagger2
-class ApplicationConfig
+class ApplicationConfig: WebMvcConfigurationSupport()
 {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry)
+    {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+            .resourceChain(false)
+    }
+
+    override fun addViewControllers(registry: ViewControllerRegistry)
+    {
+        registry.addViewController("/swagger-ui/")
+            .setViewName("forward:/swagger-ui/index.html")
+    }
+
     @Bean
     fun api(): Docket
     {
         return Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(getApiInfo())
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.fdsystem.fdserver.controller"))
             .paths(PathSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("com.fdsystem.fdserver.controller"))
             .build()
+            .apiInfo(getApiInfo())
     }
 
     private fun getApiInfo(): ApiInfo
