b3990c12847c35c87b59a6998c85a48c7a5dccdc
trvehazzk3r <dimakrok@bk.ru>
2021-10-20 18:25:18 +0300

    ADD: new token field

--- lab01/fdserver/init.sql
+++ lab01/fdserver/init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     usedId serial PRIMARY KEY,
     username VARCHAR (200) UNIQUE NOT NULL,
-    password VARCHAR (200) NOT NULL
+    password VARCHAR (200) NOT NULL,
+    dbToken VARCHAR (100) NOT NULL
 );
