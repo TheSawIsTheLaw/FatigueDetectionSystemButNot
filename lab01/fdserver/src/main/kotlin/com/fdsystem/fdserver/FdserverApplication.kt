faaa9643c64c3111afd629d928f2a54c4116f551
trvehazzk3r <dimakrok@bk.ru>
2021-10-21 20:38:09 +0300

    NOFIX: ok, springfox goes to hell

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/FdserverApplication.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/FdserverApplication.kt
@@ -3,7 +3,6 @@ package com.fdsystem.fdserver
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
 class FdserverApplication
