3a3c64122cbfc9528d01ddc4dc69b7ca04fb660b
trvehazzk3r <dimakrok@bk.ru>
2021-10-27 23:42:18 +0300

    ADD: configuration JSON file

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/config/NetworkConfig.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/config/NetworkConfig.kt
@@ -1,15 +1,48 @@
 package com.fdsystem.fdserver.config
 
+import com.google.gson.Gson
+import java.io.File
+import java.io.InputStream
+
 object NetworkConfig
 {
+    data class NetworkConfigData
+        (
+        // InfluxDB necessary info
+        val influxdbURL: String,
+        val influxAdminToken: String,
+        val influxOrganization: String,
+
+        // Postgres necessary info
+        val postgresURL: String,
+        val postgresUsername: String,
+        val postgresPassword: String
+    )
+
+    val data: NetworkConfigData
+
+    init
+    {
+        val stream: InputStream
+        try
+        {
+            stream = File("FDConf.json").inputStream()
+        }
+        catch (exc: Exception)
+        {
+            throw Exception("No configuration file")
+        }
+        val jsonToConvert = stream.bufferedReader().use { it.readText() }
+        data = Gson().fromJson(jsonToConvert, NetworkConfigData::class.java)
+    }
+
     // InfluxDB necessary info
-    val influxdbURL: String = "http://influxdb:8086"
-    val influxAdminToken: String =
-        "S2BxreP0buWYzaFZ3H6wXMdu1zuJddAon_fJzjFtq64oftC4_YaqhHlWLGobjD5n9D2dBfO3NCFAoV0EchXPOA=="
-    val influxOrganization: String = "subjects"
+    val influxdbURL: String = data.influxdbURL
+    val influxAdminToken: String = data.influxAdminToken
+    val influxOrganization: String = data.influxOrganization
 
     // Postgres necessary info
-    val postgresURL: String = "postgres:5432/users"
-    val postgresUsername: String = "admin"
-    val postgresPassword: String = "satanIsHere"
+    val postgresURL: String = data.postgresURL
+    val postgresUsername: String = data.postgresUsername
+    val postgresPassword: String = data.postgresPassword
 }
