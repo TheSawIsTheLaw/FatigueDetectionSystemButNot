a3530ca42731ca903daada8adeb0d94874da5dc4
trvehazzk3r <dimakrok@bk.ru>
2021-10-30 18:15:15 +0300

    FIX: wow. I don't know why it was there but Kotlin accepts russian keys in a classname...

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/controllers/UserController.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/controllers/UserController.kt
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpUtils
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/user")
-class АНТИАНТИПРИВИЧОНИКUserController(
+class UserController(
     val userService: UserAuthService,
 //    val authenticationManager: AuthenticationManager,
     val jwtTokenUtil: JwtTokenUtil,
