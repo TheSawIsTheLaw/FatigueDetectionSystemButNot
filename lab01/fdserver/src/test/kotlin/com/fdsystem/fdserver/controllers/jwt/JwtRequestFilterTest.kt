b9a2fc02877028b8e60874b085c3f3451b0fac4b
trvehazzk3r <dimakrok@bk.ru>
2021-11-03 13:34:54 +0300

    ADD: starts for tests of JwtRequestFilter

---
+++ lab01/fdserver/src/test/kotlin/com/fdsystem/fdserver/controllers/jwt/JwtRequestFilterTest.kt
@@ -0,0 +1,14 @@
+package com.fdsystem.fdserver.controllers.jwt
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class JwtRequestFilterTest
+{
+
+    @Test
+    fun doFilterInternal()
+    {
+    }
+}
