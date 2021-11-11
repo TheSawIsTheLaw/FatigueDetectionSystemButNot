1aed51305374a0e65df3642cbe3a86b6eec88857
trvehazzk3r <dimakrok@bk.ru>
2021-11-12 01:48:18 +0300

    FIX: imports

--- lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/controllers/jwt/JwtRequestFilter.kt
+++ lab01/fdserver/src/main/kotlin/com/fdsystem/fdserver/controllers/jwt/JwtRequestFilter.kt
@@ -8,9 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
 import javax.servlet.FilterChain
-import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
