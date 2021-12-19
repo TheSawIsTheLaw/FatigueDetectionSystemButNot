d52a54dd723063fef27fdec513c26fed3b627806
trvehazzk3r <dimakrok@bk.ru>
2021-12-19 17:02:19 +0300

    ADD: postgres for integration testing

---
+++ lab01/fdserver/integrationTests/postgres/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    userId serial PRIMARY KEY,
+    username VARCHAR (200) UNIQUE NOT NULL,
+    password VARCHAR (200) NOT NULL,
+    dbToken VARCHAR (100) NOT NULL
+);
+
+INSERT INTO users(username, password, dbtoken) VALUES
+    ('user', 'password', 'HsJBf0sINtvxedXJio2Lg7iskJgLcR5q8a0MZtqoiWZt66pBEQ0LUz0IPEe5ooD2GqaxQoGxzqoIi-U1CLINow==');
